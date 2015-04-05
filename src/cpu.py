f9a610f1a90d63fc6eb70a8428ca078f44ed8936
Vurmux <vurmux@gmail.com>
2015-04-05 23:31:43 +0300

    Added primitive working CPU class

--- src/cpu.py
+++ src/cpu.py
@@ -1,6 +1,44 @@
 #!/usr/bin/python
 
+import flag
+import flag_functions
+import functions
+import instruction
+import register
+
+
 class CPU:
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, **kwargs):
+        self.name = kwargs['name']
+        self.instructions = {i.name: i for i in kwargs['instructions']}
+        self.flags = {f.name: f for f in kwargs['flags']}
+        self.registers = {r.name: r for r in kwargs['registers']}
+        
+    def __str__(self):
+        result = ''
+        result = result + 'CPU: ' + self.name + '\n'
+        result = result + 'REGISTERS:\n'
+        for r_name in self.registers:
+            result = result + str(self.registers[r_name]) + '\n'
+        result = result + str(self.flags) + '\n'
+        result = result + str(self.instructions) + '\n'
+        result += '\n'
+        return result
+        
+    def execute(self, i_name, reg1, reg2):
+        self.instructions[i_name].execute(reg1, reg2)
+
+
+if __name__ == '__main__':
+    XOR = instruction.Instruction('XOR', functions.func_xor)
+    ZF = flag.Flag('ZF')
+    AX = register.Register('AL', size=8)
+    
+    cpu = CPU(
+        name='x86',
+        instructions=[XOR, ],
+        flags=[ZF, ],
+        registers=[AX, ],
+    )
+    print cpu
