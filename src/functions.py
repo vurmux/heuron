5dd6236c7d2db69ef61a84af9999a01ba150f595
Andrey <vurmux@gmail.com>
2015-04-11 15:31:19 +0300

    Removed bounds decorator due to new joints system

--- src/functions.py
+++ src/functions.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python
 
-import flag_functions as ff
-import flag
 
-
-def bound(function, flag):
-    def decorator(func):
-        def wrapper(op1, op2):
-            result = func(op1, op2)
-            flag.state = function(result)
-            return result
-        return wrapper
-    return decorator
-    
-
-@bound(ff.zero_function, flag.ZF)
 def func_xor(op1, op2):
     if len(op1) != len(op2):
         raise ValueError
