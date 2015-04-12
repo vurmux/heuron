8ab4041489d06b690d5a52261667645990572e27
vurmux <vurmux@gmail.com>
2015-04-12 23:18:54 +0300

    Added AND and OR functions

--- src/functions.py
+++ src/functions.py
@@ -6,3 +6,15 @@ def func_xor(op1, op2):
         raise ValueError
     res = [op1[i] ^ op2[i] for i in range(len(op1))]
     return res
+
+def func_or(op1, op2):
+    if len(op1) != len(op2):
+        raise ValueError
+    res = [op1[i] | op2[i] for i in range(len(op1))]
+    return res
+
+def func_and(op1, op2):
+    if len(op1) != len(op2):
+        raise ValueError
+    res = [op1[i] & op2[i] for i in range(len(op1))]
+    return res
