8f264ef3edfb722057ae4965bf2bbaaf4da08690
Andrey <vurmux@gmail.com>
2015-04-14 19:25:15 +0300

    Wrote basic arithmetic functions

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python
 
 
+# -- General functions ---
+
+def list_to_int(lst):
+    return sum(lst[i] * 2**(i+1) for i in range(len(lst)))
+
+def int_to_list(value):
+    bin_value = bin(value)[2: ]
+    result = []
+    for b in bin_value[:: -1]:
+        result.append(int(b))
+    return result
+
+# --- ALU functions ---
+
 def func_xor(op1, op2):
     if len(op1) != len(op2):
         raise ValueError
@@ -18,3 +32,40 @@ def func_and(op1, op2):
         raise ValueError
     res = [op1[i] & op2[i] for i in range(len(op1))]
     return res
+
+def func_nop():
+    pass
+
+def func_add(op1, op2):
+    int_op1 = list_to_int(op1)
+    int_op2 = list_to_int(op2)
+    int_result = int_op1 + int_op2
+    return int_to_list(int_result)
+
+def func_sub(op1, op2):
+    int_op1 = list_to_int(op1)
+    int_op2 = list_to_int(op2)
+    int_result = int_op1 - int_op2
+    return int_to_list(int_result)
+
+def func_mul(op1, op2):
+    int_op1 = list_to_int(op1)
+    int_op2 = list_to_int(op2)
+    int_result = int_op1 * int_op2
+    return int_to_list(int_result)
+
+def func_div(op1, op2):
+    int_op1 = list_to_int(op1)
+    int_op2 = list_to_int(op2)
+    int_result = int(float(int_op1) / int_op2)
+    return int_to_list(int_result)
+
+def func_inc(op):
+    int_op = list_to_int(op)
+    int_result = int_op + 1
+    return int_to_list(int_result)
+
+def func_dec(op):
+    int_op = list_to_int(op)
+    int_result = int_op - 1
+    return int_to_list(int_result)
