bb6e4b38e3c4e0d87b507ec8d925fd5651bea258
Andrey <vurmux@gmail.com>
2015-04-15 19:52:58 +0300

    Added actual instruction parameters to constructor

--- src/instruction.py
+++ src/instruction.py
@@ -5,8 +5,11 @@ import joint
 
 class Instruction:
     
-    def __init__(self, name, function):
+    def __init__(self, name, mnemonic, operands, result, function):
         self.name = name
+        self.mnemonic = mnemonic
+        self.operands = operands
+        self.result = result
         self.function = function
         
     def execute(self, *operands):
