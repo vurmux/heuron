fea7b8eddd68539d044856bee4b4c5492ec34265
vurmux <vurmux@gmail.com>
2015-05-02 22:34:19 +0300

    Started to write stack implementation

---
+++ src/stack.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+
+class Stack:
+    
+    def __init__(self):
+        self.stack = []
+        
+    def push(self, element):
+        self.stack.append(element)
+        
+    def pop(self, element):
+        if not self.stack:
+            raise ValueError
+        element = self.stack.pop()
