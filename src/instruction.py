34d71e8c3c38d04f18dfc5fcc4c7cbb89c48eb3f
Andrey <vurmux@gmail.com>
2015-05-08 21:03:36 +0300

    Created inherited instruction classes for instruction simplicity

--- src/instruction.py
+++ src/instruction.py
@@ -11,7 +11,7 @@ import flag
 import register
 
 
-class Instruction:
+class Instruction(object):
     
     def __init__(self, name, mnemonic, operands, result, function_name, bound):
         self.name = name
@@ -48,7 +48,7 @@ class Instruction:
                 refined_operands.append(op)
         #refined_operands = [op.value for op in operands if isinstance(op, register.Register) else op]
         result = getattr(functions, self.function_name)(*refined_operands)
-        if self.result != '-':
+        if self.result:
             operands_dict[self.result].value = result
         for joint_name, joint in self.joints.iteritems():
             if isinstance(joint.j_to, flag.Flag):
@@ -61,6 +61,42 @@ class Instruction:
 
     def set_joint(self, joint):
         self.joint = joint
+
+
+class ArithmeticInstruction(Instruction):
+    pass
+
+
+class DataTransferInstruction(Instruction):
+    pass
+
+
+class ControlTransferInstruction(Instruction):
+    
+    def __init__(self, name, mnemonic, operands, result, function_name, bound):
+        super(DataTransferInstruction, self).__init__(
+            name,
+            mnemonic,
+            operands,
+            result,
+            function_name,
+            bound
+        )
+        
+    def execute(self, *operands):
+       if len(operands) != len(self.operands):
+           raise ValueError
+       operands_dict = dict(zip(self.operands, operands))
+       refined_operands = []
+       getattr(functions, self.function_name)(*operands)
+       for joint_name, joint in self.joints.iteritems():
+           if isinstance(joint.j_to, flag.Flag):
+               joint.bend(
+                   joint_functions.set_flag,
+                   joint.j_to,
+                   getattr(flag_functions, joint.j_to.function),
+                   result
+               )
 
 
 def load_from_file(filename):
