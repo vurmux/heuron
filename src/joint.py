ac888bf785f99d7c91cee3ecdb34161d21e1240b
vurmux <vurmux@gmail.com>
2015-10-30 22:28:44 +0300

    Added short documentation to the Joint class

--- src/joint.py
+++ src/joint.py
@@ -2,6 +2,16 @@
 
 
 class Joint:
+    """
+    Implementation of Observer-like pattern.
+    This pattern is very similar to Observer pattern. The main difference
+    between them is that every Joint object is inside objects it connects
+    unlike Observer objects.
+
+    Class attributes:
+    j_from - primary object that sends functions to the secondary
+    j_to - secondary object that receives functions from the primary
+    """
 
     def __init__(self, j_from):
         self.j_from = j_from
@@ -11,10 +21,25 @@ class Joint:
         return str(self.j_from) + '->' + str(self.j_to)
 
     def connect(self, target):
+        """
+        Connect current joint to target object.
+        Every joint is created connected to one object so after calling this
+        function current joint will be connected with two objects.
+
+        Arguments:
+        target - object current joint is connected to
+        """
         self.j_to = target
         target.set_joint(self)
 
     def bend(self, func, *args):
+        """
+        Send a function from one object to another through the current joint.
+
+        Arguments:
+        func - target function
+        args - arguments of target function
+        """
         if not self.j_to:
             raise ValueError
         func(*args)
